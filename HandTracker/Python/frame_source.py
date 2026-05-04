24fa67a5e3907b9af0814e61ec8e6445ed5a3b5d
KJWybo018 <55262252+KJWybo018@users.noreply.github.com>
2026-05-04 09:19:14 -0400

    Create frame_source.py

---
+++ HandTracker/Python/frame_source.py
@@ -0,0 +1,15 @@
+import cv2
+class FrameSource:
+  def __init__(self, camera_index=0, width=1280, height=720):
+    self.cap = cv2.VideoCapture(camera_index)
+    self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+  def get_frame(self):
+    ret, frame = self.cap.read()
+    if not ret:
+      return None
+    return frame
+
+  def release(self):
+    self.cap.release()
