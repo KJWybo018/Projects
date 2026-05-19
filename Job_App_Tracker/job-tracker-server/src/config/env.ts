0acf64a1f5ba6a10cf6227703b8b4149e8358067
KJWybo018 <55262252+KJWybo018@users.noreply.github.com>
2026-05-19 13:26:39 -0400

    Update env.ts

--- Job_App_Tracker/job-tracker-server/src/config/env.ts
+++ Job_App_Tracker/job-tracker-server/src/config/env.ts
@@ -1,1 +1,6 @@
-
+export const config = {
+  jwtSecret: process.env.JWT_SECRET as string,
+  jwtExpiresIn: process.env.JWT_EXPIRES_IN || '7d',
+  clientUrl: process.env.CLIENT_URL || 'http://localhost:5173',
+  port: parseInt(process.env.PORT || '4000'),
+};
