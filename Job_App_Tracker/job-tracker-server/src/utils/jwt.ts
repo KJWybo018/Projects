02db85550ef5939f0e64f9789d6093bd742e8d62
KJWybo018 <55262252+KJWybo018@users.noreply.github.com>
2026-05-19 16:23:33 -0400

    Update jwt.ts

--- Job_App_Tracker/job-tracker-server/src/utils/jwt.ts
+++ Job_App_Tracker/job-tracker-server/src/utils/jwt.ts
@@ -1,1 +1,9 @@
+import jwt from 'jsonwebtoken';
+import { config } from '../config/env';
+import { AuthPayload } from '../types';
 
+export const signToken = (payload: AuthPayload): string =>
+  jwt.sign(payload, config.jwtSecret, { expiresIn: config.jwtExpiresIn } as jwt.SignOptions);
+
+export const verifyToken = (token: string): AuthPayload =>
+  jwt.verify(token, config.jwtSecret) as AuthPayload;
