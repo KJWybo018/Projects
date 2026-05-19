a699a17f2aac73e2dfb89e35737823e6b6182b0a
KJWybo018 <55262252+KJWybo018@users.noreply.github.com>
2026-05-19 13:26:16 -0400

    Update index.ts

--- Job_App_Tracker/job-tracker-server/src/index.ts
+++ Job_App_Tracker/job-tracker-server/src/index.ts
@@ -1,1 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes';
+import jobsRoutes from './routes/jobs.routes';
+import statsRoutes from './routes/stats.routes';
+import { errorHandler } from './middleware/errorHandler';
 
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(cors({ origin: process.env.CLIENT_URL }));
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/jobs', jobsRoutes);
+app.use('/api/stats', statsRoutes);
+
+app.use(erroeHandler);
+
+app.listen(PORT, () => {
+  console.log('Server running on port ${PORT}');
+});
