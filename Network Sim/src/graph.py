949bda36ca4c03eb338f4271520f3b1a40b3f2c4
KJWybo018 <55262252+KJWybo018@users.noreply.github.com>
2026-05-26 19:36:24 -0400

    Update graph.py

--- Network Sim/src/graph.py
+++ Network Sim/src/graph.py
@@ -1,1 +1,16 @@
+# Author: Kyle Wybo
+# Created: 05/26/2026
 
+class Edge:
+  def __init__(self, weight, target_node=None):
+    self.weight = weight
+    self.to_node = target_node
+
+
+class Node:
+  def __init__(self, data, memMax, currUse):
+    self.data = data
+    self.edges = []
+    self.distance = float('inf')
+    self.memMax = memMax
+    self.currUse = currUse
