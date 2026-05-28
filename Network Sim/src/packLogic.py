92fe352f2a57e741f64cb4d1117044a90c3bad62
KJWybo018 <55262252+KJWybo018@users.noreply.github.com>
2026-05-28 14:13:14 -0400

    Update packLogic.py

--- Network Sim/src/packLogic.py
+++ Network Sim/src/packLogic.py
@@ -1,5 +1,20 @@
 # Author: Kyle Wybo
 # Date Created: 05/28/2026
 
-class Event:
-  
+class Event():
+
+  def __init__(self, priority, event_type, packetData, nodeData):
+    self.priority = priority
+    self.event_type = event_type
+    self.packetData = packetData
+    self.nodeData = nodeData
+
+class Packet():
+
+  def __init__(self, start, end, packID, size):
+    self.start = start
+    self.end = end
+    self.packID = packID
+    self.size = size
+
+
