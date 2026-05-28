e9746485767f6235c0a10458dc4214f2aea6c74e
KJWybo018 <55262252+KJWybo018@users.noreply.github.com>
2026-05-28 13:54:36 -0400

    Update eventEngine.py

--- Network Sim/src/eventEngine.py
+++ Network Sim/src/eventEngine.py
@@ -3,4 +3,23 @@
 
 import heapq
 
-class EngineEvent:
+class EngineEvent():
+
+  def __init__(self):
+
+
+  def add_event(self, ):
+
+
+  def run(self, event):
+
+    while (!queue):
+      
+      if (event is Null):
+        pass
+      elif (event is Null):
+        pass
+      elif (event is Null):
+        pass
+
+    
